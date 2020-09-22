9fdd069494910168b165deba5bf4c1d021df977a
Val <valeria.carvajal01@correo.usa.edu.co>
2020-09-22 12:35:31 -0500

    DefinicionClasePunto

---
+++ Punto.py
@@ -0,0 +1,10 @@
+  
+import math
+
+class Punto:
+  def __init__(self,x,y):
+   self.x = x
+   self.y = y
+
+  def hallarDistancia(self,Punto):
+        return math.sqrt(math.pow(self.x- Punto.x,2)+math.pow(self.y-Punto.y,2))
