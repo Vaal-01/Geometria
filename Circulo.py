f08318e5abe1109c018e09fc642dc60d8e0b5a62
Val <valeria.carvajal01@correo.usa.edu.co>
2020-09-23 21:12:06 -0500

    ClaseCirculo

---
+++ Circulo.py
@@ -0,0 +1,28 @@
+import math
+
+from Punto import Punto
+
+class Circulo():
+
+  def __init__(self,punto,radio):
+   self.punto = punto
+   self.radio = radio
+
+   
+  def hallarPerimetro(self):
+        perimetroC=(2*math.pi*self.radio)
+        return perimetroC
+    
+
+  def hallarArea(self):
+        areaC=(math.pi*math.pow(self.radio,2))
+        return areaC
+    
+  def determinarDentro(self,otro):
+
+        if (self.punto.hallarDistancia(otro))<=self.radio:
+            val=True
+        else:
+            val=False
+
+        return val
